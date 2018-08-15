3e4e28662f28753311f78d6f8807e6c0b430a6ba
ckaatz <39869540+ckaatz@users.noreply.github.com>
2018-08-14 20:34:01 -0500

    Add project files.

---
+++ C-Step114/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Step114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NewClass Class1 = new NewClass();
+            int result1 = Class1.mathOperation(12);
+            Console.WriteLine(result1);
+        }
+    }
+
+    public class NewClass
+    {
+        public int mathOperation(int Num1)
+        {
+            return Num1 + 5;
+        }
+    }
+}
