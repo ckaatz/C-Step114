0f615071ae0ecc208cee7c4631e1dc62efc56990
ckaatz <39869540+ckaatz@users.noreply.github.com>
2018-08-14 21:18:30 -0500

    Finish program

--- C-Step114/Program.cs
+++ C-Step114/Program.cs
@@ -11,16 +11,48 @@ namespace C_Step114
         static void Main(string[] args)
         {
             NewClass Class1 = new NewClass();
-            int result1 = Class1.mathOperation(12);
+            int result1 = Class1.MathOperation(12);
             Console.WriteLine(result1);
+            Console.ReadLine();
+
+            NewClass Class2 = new NewClass();
+            decimal NotAnInt = 1.42m;
+            decimal result2 = Class2.MathOperation(NotAnInt);
+            Console.WriteLine(result2);
+            Console.ReadLine();
+
+            NewClass Class3 = new NewClass();
+            int result3 = Class3.MathOperation("5");
+            Console.WriteLine(result3);
+            Console.ReadLine();
         }
     }
 
     public class NewClass
     {
-        public int mathOperation(int Num1)
+        public int MathOperation(int Num1)
         {
             return Num1 + 5;
+        }
+
+        public int MathOperation(decimal Num2)
+        {
+            int Num3 = Convert.ToInt32(Num2 * 100);
+            return Num3;
+        }
+
+        public int MathOperation(string Num4)
+        {
+            try
+            {
+                int Num5 = Convert.ToInt32(Num4) - 2;
+                return Num5;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("That is not a number. Here is a number: ");
+                return 5;
+            }
         }
     }
 }
